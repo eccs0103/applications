ce0effb5bb77263dfd952f4029a8463f67e413fc
Arman Sargsyan <arman.sargsyan2@nrgaming.com>
2026-07-29 15:55:55 +0400

    1.0.0

---
+++ 209-birthdays/models/settings.ts
@@ -0,0 +1,11 @@
+"use strict";
+
+import "adaptive-extender/core";
+import { Field, Model } from "adaptive-extender/core";
+
+//#region Settings
+export class Settings extends Model {
+	@Field(Number, { name: "selection" })
+	selection: number = 0;
+}
+//#endregion
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+"use strict";
+
+import { defineConfig } from "vite";
+import { type ViteConfig } from "./environment/configs/vite-config.js";
+import { MPAConfig } from "./environment/configs/mpa-config.js";
+import { CloudflareVitePlugin } from "./environment/plugins/cloudflare-vite-plugin.js";
+import { type VitePlugin } from "./environment/plugins/vite-plugin.js";
+
+const root: URL = new URL(import.meta.url);
+const inputs: URL[] = [
+	new URL("./209-birthdays/index.html", root),
+];
+const rootEntries: URL[] = [];
+const pathEntries: URL[] = [];
+const output: URL = new URL("./dist", root);
+const plugins: VitePlugin[] = [new CloudflareVitePlugin()];
+const config: ViteConfig = await MPAConfig.construct(inputs, rootEntries, pathEntries, output, plugins);
+export default defineConfig(config.build());
