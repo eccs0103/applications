46daca286d79dee55c23adc3fca705c93ba45482
Arman <eccs0103@gmail.com>
2026-09-10 15:28:34 +0400

    1.1.4

--- environment/configs/vite-config.ts
+++ environment/configs/vite-config.ts
@@ -4,6 +4,7 @@ import "adaptive-extender/node";
 import { type InputOption, type OutputOptions, type PreRenderedChunk, type RollupOptions } from "rollup";
 import { type AppType, type BuildEnvironmentOptions, type ESBuildOptions, type ServerOptions, type UserConfig } from "vite";
 import { VitePlugin } from "../plugins/vite-plugin.js";
+import { RootEntryDevPlugin } from "../plugins/root-entry-dev-plugin.js";
 import URLUtilities from "node:url";
 
 //#region Vite config
@@ -44,6 +45,19 @@ export class ViteConfig {
 			const filename = path.replace(/\\/g, "/").split("/").pop()!;
 			const name = filename.replace(/\.[^/.]+$/, String.empty);
 			entries[name] = path;
+		}
+		return entries;
+	}
+
+	#normalizeServiceWorkerDevEntries(): Map<string, string> {
+		const root = `${process.cwd().replace(/\\/g, "/")}/`;
+		const entries = new Map<string, string>();
+		for (const url of this.#rootEntries) {
+			const path = URLUtilities.fileURLToPath(url).replace(/\\/g, "/");
+			const filename = path.split("/").pop()!;
+			const name = filename.replace(/\.[^/.]+$/, String.empty);
+			const relative = path.replace(root, String.empty);
+			entries.set(`/${name}.js`, `/${relative}`);
 		}
 		return entries;
 	}
@@ -113,7 +127,8 @@ export class ViteConfig {
 		const server: ServerOptions = this.#buildServer();
 		const esbuild: ESBuildOptions = this.#buildESBuild();
 		const worker = this.#buildWorker();
-		const plugins = this.#plugins.map(plugin => plugin.build());
+		const devPlugin = new RootEntryDevPlugin(this.#normalizeServiceWorkerDevEntries());
+		const plugins = [...this.#plugins, devPlugin].map(plugin => plugin.build());
 		return { base, appType, publicDir, build, server, esbuild, worker, plugins };
 	}
 }
